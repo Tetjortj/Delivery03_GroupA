6b9b0e3ae335282950743e38e412ad0625cca065
Aleix Damora Miras <82681751+AleixDamoraMiras@users.noreply.github.com>
2025-02-24 21:10:39 +0100

    Cambios generales

---
+++ Assets/Scripts/Inventories/ItemTypes/ItemWeapons.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Inventory System/Items/Weapons")]
+public class ItemWeapons : ItemBase
+{
+    public int damage;
+}
---
+++ Assets/Scripts/Inventories/UIManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+//     public InventoryManager inventoryManager;
+//     public TextMeshProUGUI playerCoinsText;
+
+//     void Update()
+//     {
+//         playerCoinsText.text = $"Monedas: {inventoryManager.playerCoins}";
+//     }
+}
+
