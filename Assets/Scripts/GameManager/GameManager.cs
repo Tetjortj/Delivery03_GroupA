7f02501ffb14777776903425c9b8f5aab71c4d9c
agonzalezgons <166504379+agonzalezgons@users.noreply.github.com>
2025-02-24 19:07:59 +0100

    Cambio de escenario al principio

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -3,14 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    /*public void LoadScene(string sceneName)
     {
+        Debug.Log("bucle");
         SceneManager.LoadScene(sceneName);
     }
 
     public void ExitGame()
     {
         Application.Quit();
-    }
+    }*/
 }
 
